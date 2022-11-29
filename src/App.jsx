3d47ddbf190b8302dad2032c529c487a9c16d475
vanjaandrejic <vanjaandrejic@gmail.com>
2022-11-29 15:02:33 +0100

    setup dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Route } from "react-router-dom";
 import Welcome from "./Pages/Welcome";
 import Products from "./Pages/Products";
+import MainHeader from "./Components/MainHeader";
+import ProductDetail from "./Pages/ProductDetail";
 
 function App() {
   return (
     <>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
     </>
   );
 }
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,14 @@
 const Products = () => {
-    return <h1>The Products Page</h1>;
-  };
-  
-  export default Products;
-  
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>Online Course</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
